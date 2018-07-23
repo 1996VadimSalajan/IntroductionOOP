ea86864e02ce05159b089379b431332d26e4e102
Vadim Salajan <vadim.salajan@outlook.com>
2018-07-23 17:58:22 +0300

    first

---
+++ ConsoleApp9/ConsoleApp9/BookRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class BookRepository : IRepository<Book>
+    {
+            
+        List<Book> list = new List<Book>();
+     
+        public IEnumerable<Book> List
+        {
+            get
+            {
+                return list;
+            }
+        }
+        public void Add(Book entity)
+        {
+            list.Add(entity);
+        }
+        public void Delete(Book entity)
+        {
+            list.Remove(entity);
+        }
+        public void Update(Book entity,Book newentity)
+        {
+            entity = list.FirstOrDefault(x => x == newentity);
+      
+        }
+      
+    }
+}
---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IRepository<Book> book = new BookRepository();
+            List<Book> listhistory = new List<Book>();
+            var a1 = new Book() { Id = 1, Name = "Alexandr Macedonski",Type="History" };
+            List<Book> listdetective = new List<Book>();
+            var a2 = new Book() { Id = 2, Name = "Sherlock Holmes" };
+            List<Book> listromanian = new List<Book>();
+            var a3 = new Book() { Id = 3, Name = "Rio" };
+            List<Book> listdrama = new List<Book>();
+            var a4 = new Book() { Id = 4, Name = "Doctor House" };
+            IDictionary<string,List<Book>> dict = new Dictionary<string, List<Book>>()
+                                            {
+                                                {"History",listhistory},
+                                                
+                                            };
+            Books value =Books.History;
+            switch (value)
+            {
+                case Books.History:
+                    foreach (KeyValuePair<string,List<Book>> item in dict)
+                    {
+                        Console.WriteLine("Key: {0}, Value: {1}", item.Key);
+                        foreach()
+                    }
+                    break;
+                case Books.Detective:
+                    
+                    break;
+                case Books.Romanian:
+                  
+                    break;
+                case Books.Drama:
+
+                    break;
+            }
+
+            Console.ReadKey();
+
+        }
+       
+       public   enum Books
+        {
+          History,
+          Detective,
+          Romanian,
+          Drama,
+        };
+        
+      
+    }
+}
