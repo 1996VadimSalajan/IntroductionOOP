aebb2f9bc5f294b4a04b6dfb37a9c3607ac68288
Vadim Salajan <vadim.salajan@outlook.com>
2018-09-05 18:36:18 +0300

    dsfdsfsfs

---
+++ WebApplication3/WebApplication3/Models/Domain/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+   public  class Address
+    {
+        public int Id { get; set; }
+        public string Village { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
